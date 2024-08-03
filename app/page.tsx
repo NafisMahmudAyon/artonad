ed160faf93d1cd2a20e0db66fe7aeab29f37dce2
AYON <nafismahmudayon@gmail.com>
2024-08-03 11:58:28 +0600

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -10,20 +10,20 @@ export default function Home() {
 
   return (
     <main className="px-3 md:px-6 lg:px-10 pt-5 lg:pt-10 w-full lg:max-w-[1400px] mx-auto relative">
-      <h2 className="text-5xl text-center font-bold">আর্তনাদ</h2>
-      <h3 className="text-xl text-center pb-10">২৪-এর আর্তনাদ</h3>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <h2 className="text-5xl text-center text-white">আর্তনাদ</h2>
+      <h3 className="text-xl text-center pb-10 text-white">২৪-এর আর্তনাদ</h3>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 poppins-regular">
         {Story.map((story, i) => {
           return (
             <div
               key={i}
-              className="relative bg-gray-600 rounded-lg overflow-hidden cursor-pointer transform transition-all hover:scale-105"
+              className="relative bg-gray-600 rounded-lg overflow-hidden cursor-pointer transform transition-all hover:scale-105 "
               onClick={() => { setHide(false); setIndex(i); }}
             >
               <div className="relative w-full h-64 lg:h-52">
                 <Image
                   className="object-cover"
-                  src={story.img[0]}
+                  src={story.image}
                   alt={story.title}
                   layout="fill"
                   objectPosition="top"
@@ -36,9 +36,13 @@ export default function Home() {
           );
         })}
       </div>
+      <div className="mt-10 text-center text-white pb-6">
+        <p>এটি কেবল শুরু। এই আর্তনাদের তালিকা লম্বা হতে থাকবে।</p>
+        <p className="mt-4 text-xl">আমরা সকল শহীদের মাগফিরাত কামনা করি।</p>
+      </div>
       <div className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 backdrop-blur-md flex items-center justify-center z-50 overflow-auto ${hide ? "hidden" : "block"}`}>
-        <div className="bg-white p-6 rounded-lg max-w-4xl w-full relative h-[90%] overflow-y-auto">
-          <span className="absolute top-4 right-4 cursor-pointer text-gray-500 hover:text-gray-800" onClick={() => setHide(true)}>X</span>
+        <div className=" p-4 rounded-lg max-w-4xl w-full relative h-[90%] overflow-y-auto">
+          <span className="absolute top-4 right-4 cursor-pointer text-red-500 text-xl font-bold p-2 rounded-full  hover:text-white hover:bg-red-500 border border-red-500 w-7 h-7 flex items-center justify-center leading-none" onClick={() => setHide(true)}>&times;</span>
           <SingleStory index={index} />
         </div>
       </div>
--- components/SingleStory.tsx
+++ components/SingleStory.tsx
@@ -11,18 +11,18 @@ const SingleStory: React.FC<SingleStoryProps> = ({ index }) => {
   return (
     <div className="p-4 max-w-4xl mx-auto bg-white rounded-lg shadow-md">
       <h1 className="text-2xl font-bold mb-4">{story.title}</h1>
-      <p className="text-gray-600 mb-2">By {story.author} on {story.date}</p>
-      <p className="text-gray-800 mb-4">
+      <p className="text-gray-600 mb-2 poppins-regular text-xs">By {story.author} on {story.date}</p>
+      <p className="text-gray-800 mb-4 ">
         <a href={story.sourceUrl} className="underline text-blue-600" target="_blank" rel="noopener noreferrer">
           {story.source}
         </a>
       </p>
       {story.quote.length > 0 &&
 
-        <blockquote className="italic bg-gray-100 p-4 mb-4 border-l-4 border-gray-300">{story.quote}</blockquote>
+        <blockquote className="bg-gray-100 p-4 mb-4 border-l-4 border-gray-300 poppins-regular-italic">{story.quote}</blockquote>
       }
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
-        {story.img.map((src, i) => (
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4 poppins-regular ">
+        {story?.images.map((src, i) => (
           <div key={i} className="relative h-64 w-full">
             <Image
               src={src}
@@ -31,11 +31,11 @@ const SingleStory: React.FC<SingleStoryProps> = ({ index }) => {
               objectFit="cover"
               className="rounded-md"
             />
-            <p className="absolute bottom-0 bg-black bg-opacity-50 text-white text-xs p-2 rounded-b-md w-full">{story.imgCaption[i]}</p>
+            <p className="absolute bottom-0 bg-black bg-opacity-50 poppins-regular text-white text-xs p-2 rounded-b-md w-full">{story.imgCaption[i]}</p>
           </div>
         ))}
       </div>
-      <div className="text-gray-800 space-y-4">
+      <div className="text-gray-800 space-y-4 poppins-regular text-sm">
         {story.story.map((paragraph, i) => (
           <p key={i}>{paragraph}</p>
         ))}
